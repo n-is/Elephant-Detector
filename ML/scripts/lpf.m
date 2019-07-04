0af035cdb3f7ff6db14632e6b5339a34be2ef31e
n-is <073bex422.nischal@pcampus.edu.np>
2019-07-04 22:34:35 +0545

    Add Simple IIR filter - Exponential Smoothing.

---
+++ ML/scripts/lpf.m
@@ -0,0 +1,20 @@
+% Transfer Function of RC circuit
+% H = 1 / (1 + sRC)
+
+function [out] = lpf(in, time, fc)
+% Apply Discrete Filter to data
+
+    out = zeros(size(in));
+    out(1) = in(1);
+    
+    RC = 1 / (2*pi*fc);
+    
+    for i = 2:length(in)
+        dt = time(i) - time(i-1);
+        alpha = dt / (RC + dt);
+        
+        out(i) = alpha * in(i) + (1 - alpha) * out(i-1);
+    end
+
+end
+
